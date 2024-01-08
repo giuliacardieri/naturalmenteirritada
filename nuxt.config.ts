e1e56714198294906490263617b9e3c6ab58f280
Giulia Cardieri <giulia.cardieri@gmail.com>
2024-01-07 20:40:09 -0500

    Include static sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,19 @@ export default defineNuxtConfig({
       apiUrl: "https://shrouded-chamber-44012-cb885503a80c.herokuapp.com/api",
     },
   },
-  modules: ["dayjs-nuxt"],
+  modules: ["dayjs-nuxt", "@nuxtjs/sitemap"],
+  sitemap: {
+    hostname: "https://naturalmenteirritada.blog",
+    routes: [
+      "/post/chicxulub-o-asteroide-dos-dinossauros",
+      "/post/o-tsunami-no-sudeste-asiatico",
+      "/post/a-erupcao-de-eyjafjallajokull",
+      "/post/entendendo-mais-sobre-vulcoes",
+      "/post/o-tornado-de-indaiatuba",
+      "/post/porque-tornados-acontecem",
+      "/post/o-que-causa-um-tsunami",
+    ],
+  },
   dayjs: {
     locales: ["pt"],
     defaultLocale: "pt",
